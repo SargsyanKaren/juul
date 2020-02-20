30aa174ef32b43fd7d5dfc1db80f5cf3004fb9d7
Karen Sargsyan <ksarg9618@gmail.com>
2020-02-20 11:31:15 +0400

    added server

--- server/server.js
+++ server/server.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const cors = require('cors');
+const { Client } = require('pg');
+
+require('dotenv').config();
+
+const PORT = process.env.NODE_PORT || 4040;
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+const client = new Client();
+
+client.connect();
+
 app.get('/test', (req, res) => {
     console.log('Hello');
-    
+    res.end();
 })
 
-app.listen(4000);
+app.listen(PORT);
